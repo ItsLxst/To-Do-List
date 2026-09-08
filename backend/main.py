6bfd7b97bb6488fed8f9e03e54212a2f7f2e74ee
ItsLxst <169390832+ItsLxst@users.noreply.github.com>
2026-09-08 15:23:26 +0300

    add CORS settings

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(title="To-Do API")
 
 # CORS middleware permission to communicate with Next.js
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Pydantic Schemas
 
